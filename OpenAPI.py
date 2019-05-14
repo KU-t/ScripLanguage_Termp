964fd30abea6c6c96980ab09181be60da3f3358c
snrn2426@kpu.ac.kr <jhltk2426@naver.com>
2019-05-14 15:55:30 +0900

    190514 OpenAPI bacecode

--- OpenAPI.py
+++ OpenAPI.py
@@ -1,9 +1,9 @@
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus, unquote
 
-decode_key = unquote('키 입력')
+decode_key = unquote('xZ%2ByjfoWhIOr7s%2BJ0QG0HbPyNRNi46%2F4l8g7G5qTQp6IgeYNACJFFvSQe%2FEgAKR09JsMDhLWpLdyHpYibXU0bQ%3D%3D')
 
-url = 'URL주소'
+url = 'http://apis.data.go.kr/1320000/SearchMoblphonInfoInqireService/getMoblphonAcctoKindAreaPeriodInfo'
 queryParams = '?' + urlencode({ quote_plus('ServiceKey') : decode_key, quote_plus('pageNo') : '1', quote_plus('numOfRows') : '2', quote_plus('title') : '1987', quote_plus('rtNo') : '2017-MF02149', quote_plus('aplcName') : '주식회사 우정필름' })
 
 request = Request(url + queryParams)
